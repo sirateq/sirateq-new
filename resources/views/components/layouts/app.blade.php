2f5872fad1be6ed3f6a31e2906d9be6794c3be16
zaakigh <36520838+zaakigh@users.noreply.github.com>
2026-05-07 22:46:03 +0000

    Add Google Tag Manager integration for enhanced tracking
    - Included the Google tag (gtag.js) script for improved analytics tracking.
    - Configured the gtag with the specified tracking ID to enable data collection.
    - Ensured the script is loaded asynchronously to optimize page performance.

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -48,6 +48,16 @@
     })(window,document,'script','dataLayer','GTM-PM693Z3P');</script>
     <!-- End Google Tag Manager -->
  
+
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-18146464872"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-18146464872');
+</script>
 </head>
 
 <body id="body">
